ee17e037012f4d041b7fbad893ad891b3b4a558b
Mai Nguyen <mai.nguyen.gl@gmail.com>
2021-03-01 14:27:06 +0100

    Stupid Coaching

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  # verb "url", to: "controller#action"
+  get "/ask", to: "questions#ask"
+  get "/answer", to: "questions#answer"
 end
